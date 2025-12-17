cc89faaa1463904b429c268cb155c0260420568d
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-17 21:26:24 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ function App() {
 
 
   return (
-    <section className="min-h-screen bg-slate-950 text-white overflow-hidden">
-      
+    <section className="min-h-screen bg-slate-950 text-white overflow-hidden ">
+      <div className='flex flex-col gap-15 items-center justify-center'>
+        <div className='flex flex-row gap-3'>
+          <img src='reactLogo' className='h-5 w-5'></img>
+          <span className='text-xl text-blue-200'>Webinar.gg</span>
+        </div>
+        
+      </div>
     </section>
   );
 }
