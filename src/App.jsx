b7cf9a03a39a4e08cbd643af7b9d76c99d228751
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-17 21:20:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+
+  return (
+    <section className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      
+    </section>
+  );
+}
+
+export default App;
